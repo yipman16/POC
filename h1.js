4a4e65022bb419e41f2e1d9f2a5a3841e72c247c
Mohamed Ebrahem <aalmsidy1@gmail.com>
2024-09-22 18:24:04 +0300

    Update h1.js

--- h1.js
+++ h1.js
@@ -1,6 +1,6 @@
 // Replace the content of the <html> element
 document.documentElement.innerHTML = `
-<h1>Asdad</h1>
+<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Full Screen Frame</title><style>html, body {margin: 0; padding: 0; height: 600px; overflow: hidden;}</style></head><body><h1>adad</h1></body></html>
 `;
 
 // Force the browser to reparse the <head> section to apply styles and other meta tags
