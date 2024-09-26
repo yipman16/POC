7029f04d33ce82532df6662444147ef01cc4e7c2
yipman16 <83105601+yipman16@users.noreply.github.com>
2024-09-26 08:36:42 +0300

    Update h1.js

--- h1.js
+++ h1.js
@@ -1,14 +1,14 @@
 
 // Open a new window
-let newWindow = window.open('https://ap-southeast-1.console.aws.amazon.com/asdasdad&test=sss', '_blank');
+let newWindow = window.open('https://ap-southeast-1.console.aws.amazon.com/cke/auth?challenge=iltHdRa_U_6QKy3IB-yRxCqjb56eEpB3b17G7hx0YBo&redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6Il82aFpFZmt4VHF5akZxNDJTM2VPOWcifQ&region=ap-southeast-1', '_blank');
 
 // Check if the new window was successfully created
 if (newWindow) {
-  // Wait for the new window to load
-  newWindow.addEventListener('load', function () {
+  // Wait for 5 seconds before accessing the URL of the new window
+  setTimeout(function () {
     // Access the URL of the new window
     console.log("The URL of the new window is: " + newWindow.location.href);
-  }, { once: true });
+  }, 5000); // 5000 milliseconds = 5 seconds
 } else {
   console.log("New window could not be opened. Make sure pop-ups are allowed.");
 }
