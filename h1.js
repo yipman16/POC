e01483fcee71b0644bba5a0eefeeef6555bd6c2d
yipman16 <83105601+yipman16@users.noreply.github.com>
2024-09-26 09:38:24 +0300

    Update h1.js

--- h1.js
+++ h1.js
@@ -1,6 +1,6 @@
 
 // Open a new window
-let newWindow = window.open('https://ap-southeast-1.console.aws.amazon.com/cke/auth?redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6IlQyVEFpNVpBUVkyZ2E0MlFjWkpyR3cifQ&challenge=v3NAhwtOXRkuLb8FGxHtSfLiyAjWcL5s9VeN-YcRjCE&region=ap-southeast-1', '_blank');
+let newWindow = window.open('https://us-west-2.console.aws.amazon.com/cke/auth?region=us-west-2&redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6IlRUYmxzUEpfU2ItQzJNZEpsNUh3WVEifQ&challenge=erphyp4RzErFDGDcHUFFQstSu7Px0geQ_c0b6Je-boI', '_blank');
 
 // Check if the new window was successfully created
 if (newWindow) {
